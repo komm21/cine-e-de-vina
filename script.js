7276e2b374f7e2fce1fe92f060abc71f591152a4
komm21 <123317039+komm21@users.noreply.github.com>
2023-01-23 21:59:34 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-let numeleTau = document.getElementById("numeleTau");
-let numelePer2 = document.getElementById("numelePer2");
+let numeleTau = document.getElementById("numeleTau").value;
+let numelePer2 = document.getElementById("numelePer2").value;
 let btnSubmit = document.getElementById("btnSubmit");
+
+let numeCorecte = ["Cosmin", "Gabi", "Gabriela", "Calin", "cosmin", "calin", "gabi", "gabriela", "Chong", "George"]
 
 btnSubmit.addEventListener("click", adaugaText);
 
+function checkName(){
+    if (numeCorecte.includes(numeleTau)){
+        document.getElementById("nume1Incorect").innerHTML="Nume corect! Felicitari iti stii numele.";
+    }else{
+        document.getElementById("nume1Incorect").innerHTML="Nume incorect. Hmmm...";
+    }
+    
+    if(numeCorecte.includes(numelePer2)){
+        document.getElementById("nume2Incorect").innerHTML="Nume corect. Haleluia";
+    }else{
+        document.getElementById("nume2Incorect").innerHTML="Nume incorect. Mai incearca.";
+    }
+}
+
 function adaugaText(){
+    checkName();
     let text = "Gabi";
     document.getElementById("persVinovata").innerHTML=text;
-    return
 }
