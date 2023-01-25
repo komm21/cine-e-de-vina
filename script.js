3dcdbbd28020aeade48277d8f51542e2fa1048cb
komm21 <123317039+komm21@users.noreply.github.com>
2023-01-25 19:22:19 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-let numeleTau = document.getElementById("numeleTau").value;
-let numelePer2 = document.getElementById("numelePer2").value;
-let btnSubmit = document.getElementById("btnSubmit");
-let stabPersVin = document.getElementById("stabPersVin");
+const numeleTau = document.getElementById("numeleTau");
+const numelePer2 = document.getElementById("numelePer2");
+const btnSubmit = document.getElementById("btnSubmit");
+const stabPersVin = document.getElementById("stabPersVin");
+const btnRefresh = document.getElementById("refresh");
 
 btnSubmit.addEventListener("click", checkName);
 stabPersVin.addEventListener("click", adaugaText);
+btnRefresh.addEventListener("click", onReset);
 
-let numeCorecte = ["Cosmin", "Gabi", "Gabriela", "Calin", "cosmin", "calin", "gabi", "gabriela", "Chong", "George"]
 
-
+const numeCorecte = ["Cosmin", "Gabi", "Gabriela", "Calin", "cosmin", "calin", "gabi", "gabriela", "Chong", "George"];
 
 
 function checkName(){
-    if (numeCorecte.includes(numeleTau)){
+    if (numeCorecte.indexOf(numeleTau.value) !=-1){
         document.getElementById("nume1Incorect").innerHTML="Nume corect! Felicitari iti stii numele.";
     }else{
         document.getElementById("nume1Incorect").innerHTML="Nume incorect. Hmmm...";
     }
     
-    if(numeCorecte.includes(numelePer2)){
+    if(numeCorecte.indexOf(numelePer2.value) !=-1){
         document.getElementById("nume2Incorect").innerHTML="Nume corect. Haleluia";
     }else{
         document.getElementById("nume2Incorect").innerHTML="Nume incorect. Mai incearca.";
@@ -27,8 +28,13 @@ function checkName(){
 
 let x = Math.floor(Math.random()*numeCorecte.length);
 
-
 function adaugaText(){
     let text = "In urma unor analize complicate, persoana vinovata este..."+ numeCorecte[x];
     document.getElementById("persVinovata").innerHTML=text;
+    document.getElementById("refresh").disabled=false;
 }
+
+function onReset(){
+    window.location.reload();
+}
+
